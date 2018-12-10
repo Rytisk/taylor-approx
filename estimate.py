993235bfb6ea13a5021ff62570a6624559625ed2
rytisk <rytis.kap@gmail.com>
2018-12-10 18:49:31 +0200

    estimation upd

--- estimate.py
+++ estimate.py
@@ -6,26 +6,27 @@ plt.style.use("ggplot")
 
 # Factorial function
 def factorial(n):
-    if n <= 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+	if n <= 0:
+		return 1
+	else:
+		return n * factorial(n - 1)
 
 # Taylor approximation at x0 of the function 'function'
 def taylor(function, x0, n, x = sy.Symbol('x')):
-    i = 0
-    p = 0
-    while i <= n:
-        p = p + (function.diff(x, i).subs(x, x0))/(factorial(i))*(x - x0)**i
-        i += 1
-    return p
+	i = 0
+	p = 0
+	while i <= n:
+		p = p + (function.diff(x, i).subs(x, x0))/(factorial(i))*(x - x0)**i
+		i += 1
+	return p
 
-def lagrange(function, x0, n, x = sy.Symbol('x')):
-    i = n + 1
-    p = (function.diff(x, i).subs(x, x0))/(factorial(i))*(x - x0)**i
-    return p
+def lagrange(function, x0, n, x = sy.Symbol('x'), E = sy.Symbol('E')):
+	i = n + 1
+	p = (function.diff(x, i).subs(x, E))/(factorial(i))*(x - x0)**i
+	return p
 
 x = sy.Symbol('x')
+E = sy.Symbol('E')
 f = exp(x) * ln(1 + x)
 
 func = taylor(f, 0, 5)
@@ -33,6 +34,8 @@ taylor_lambda = sy.lambdify(x, func, "numpy")
 print('Taylor expansion at n=' + str(5), func)
 
 lagr = lagrange(f, 0, 5)
-print('Lagrange remainder at n=' + str(5), lagr)
+lagr_lambda = sy.lambdify((x,E), lagr, "numpy")
+print('Lagrange remainder', lagr)
 
-#plt.plot(x1, taylor_lambda(x1), label = 'Order '+ str(j))
+print('\n')
+print(lagr_lambda(1, 0))
--- main.py
+++ main.py
@@ -12,43 +12,43 @@ f = exp(x) * log(1+x)
 
 # Factorial function
 def factorial(n):
-    if n <= 0:
-        return 1
-    else:
-        return n*factorial(n-1)
+	if n <= 0:
+		return 1
+	else:
+		return n*factorial(n-1)
 
 # Taylor approximation at x0 of the function 'function'
 def taylor(function,x0,n):
-    i = 0
-    p = 0
-    while i <= n:
-        p = p + (function.diff(x,i).subs(x,x0))/(factorial(i))*(x-x0)**i
-        i += 1
-    return p
+	i = 0
+	p = 0
+	while i <= n:
+		p = p + (function.diff(x,i).subs(x,x0))/(factorial(i))*(x-x0)**i
+		i += 1
+	return p
 
 # Plot results
 def plot():
-    x_lims = [-1/2,2]
-    x1 = np.linspace(x_lims[0],x_lims[1],800)
-    y1 = []
-    # Approximate up until 10 starting from 1 and using steps of 2
-    for j in range(1,6,1):
-        func = taylor(f,0,j)
-        print('Taylor expansion at n='+str(j),func)
-        for k in x1:
-            y1.append(func.subs(x,k))
-        plt.plot(x1,y1,label='order '+str(j))
-        y1 = []
-    # Plot the function to approximate
-    fs = list(map(lambda x0: math.exp(x0) * math.log(1+x0), x1))
-    plt.plot(x1, fs,label='f of x')
-    plt.xlim(x_lims)
-    plt.ylim([-1/2,2])
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend()
-    plt.grid(True)
-    plt.title('Taylor series approximation')
-    plt.show()
+	x_lims = [-1/2,2]
+	x1 = np.linspace(x_lims[0],x_lims[1],800)
+	y1 = []
+	# Approximate up until 10 starting from 1 and using steps of 2
+	for j in range(1,6,1):
+		func = taylor(f,0,j)
+		print('Taylor expansion at n='+str(j),func)
+		for k in x1:
+			y1.append(func.subs(x,k))
+		plt.plot(x1,y1,label='order '+str(j))
+		y1 = []
+	# Plot the function to approximate
+	fs = list(map(lambda x0: math.exp(x0) * math.log(1+x0), x1))
+	plt.plot(x1, fs,label='f of x')
+	plt.xlim(x_lims)
+	plt.ylim([-1/2,2])
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.legend()
+	plt.grid(True)
+	plt.title('Taylor series approximation')
+	plt.show()
 
 plot()
